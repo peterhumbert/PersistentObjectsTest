93db8065babfed90c55d6bea011346f7eebb3cf1
Riley Hillstrom <Riley.hillstrom@gmail.com>
2017-03-02 10:51:05 -0600

    Implemented Basic GUI Elements
    Added versions drop down and submit button

--- Converter/Form1.Designer.cs
+++ Converter/Form1.Designer.cs
@@ -28,24 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.cboVersions = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // button1
+            // btnSubmit
             // 
-            this.button1.Location = new System.Drawing.Point(95, 105);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSubmit.Location = new System.Drawing.Point(145, 152);
+            this.btnSubmit.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(56, 19);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "ofdOpenFile";
+            // 
+            // cboVersions
+            // 
+            this.cboVersions.FormattingEnabled = true;
+            this.cboVersions.Items.AddRange(new object[] {
+            "Version 1",
+            "Version 2",
+            "Current Version"});
+            this.cboVersions.Location = new System.Drawing.Point(38, 12);
+            this.cboVersions.Name = "cboVersions";
+            this.cboVersions.Size = new System.Drawing.Size(121, 21);
+            this.cboVersions.TabIndex = 1;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(212, 206);
+            this.Controls.Add(this.cboVersions);
+            this.Controls.Add(this.btnSubmit);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,7 +75,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ComboBox cboVersions;
     }
 }
 
--- Converter/Form1.cs
+++ Converter/Form1.cs
@@ -16,5 +16,6 @@ namespace Converter
         {
             InitializeComponent();
         }
+
     }
 }
